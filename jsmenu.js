1459db5ee3e6682c91897177980bb70a04419de9
Nicolecuadrav <nicolecuadravenegas@gmail.com>
2023-07-17 04:04:42 -0400

    Se añadio boton desplegable y la mayoría de destalles esteticos

--- jsmenu.js
+++ jsmenu.js
@@ -3,9 +3,26 @@ const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
+     nav.classList.add("visible");
+ })
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+ cerrar.addEventListener("click", () => {
+     nav.classList.remove("visible");
+ })
+
+// Aperturas sub menus 
+
+ let listElements = document.querySelectorAll('.list__button--click');
+ listElements.forEach(listElement =>{
+    listElement.addEventListener('click', ()=>{
+        listElement.classList.toggle('arrow');
+
+        let height=0;
+        let menu= listElement.nextElementSibling;
+        if (menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+    })
+ })
